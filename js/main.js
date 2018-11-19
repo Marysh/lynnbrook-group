7beca36c6685b1a262e71658e971ddc750d0feb0
Marysh <35464372+Marysh@users.noreply.github.com>
2018-11-19 07:56:52 +0200

    inital

--- js/main.js
+++ js/main.js
@@ -6,46 +6,7 @@ $(function () { // dom ready
             prevEl: '.swiper-button-prev'
         }
     });
-    //
-    // var swiper2 = new Swiper('#panel-2 .swiper-container', {
-    //     loop: true,
-    //     navigation: {
-    //         nextEl: '#panel-2 .swiper-button-next',
-    //         prevEl: '#panel-2 .swiper-button-prev'
-    //     }
-    // });
-    //
-    // var swiper3 = new Swiper('#panel-3 .swiper-container', {
-    //     loop: true,
-    //     navigation: {
-    //         nextEl: '#panel-3 .swiper-button-next',
-    //         prevEl: '#panel-3 .swiper-button-prev'
-    //     }
-    // });
-    //
-    // var swiper4 = new Swiper('#panel-4 .swiper-container', {
-    //     loop: true,
-    //     navigation: {
-    //         nextEl: '#panel-4 .swiper-button-next',
-    //         prevEl: '#panel-4 .swiper-button-prev'
-    //     }
-    // });
-    //
-    // var swiper5 = new Swiper('#panel-5 .swiper-container', {
-    //     loop: true,
-    //     navigation: {
-    //         nextEl: '#panel-5 .swiper-button-next',
-    //         prevEl: '#panel-5 .swiper-button-prev'
-    //     }
-    // });
-    //
-    // var swiper6 = new Swiper('#panel-6 .swiper-container', {
-    //     loop: true,
-    //     navigation: {
-    //         nextEl: '#panel-6 .swiper-button-next',
-    //         prevEl: '#panel-6 .swiper-button-prev'
-    //     }
-    // });
+
 
     $('.tabs_captions .tabs_caption').click(function () {
         var tab_id = $(this).attr('data-tab');
@@ -57,17 +18,5 @@ $(function () { // dom ready
         $("#" + tab_id).addClass('current');
     })
 
-
-    // var object2 = {
-    //     "name": [5,5,5],
-    //     "age": 25,
-    //     "car": "kangoo",
-    //     "family" : {
-    //         "mother" : {
-    //             "age": "Nataly",
-    //             age : 40
-    //         }
-    //     }
-    // }
 });
 
